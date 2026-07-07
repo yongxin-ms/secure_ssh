65e9aa4af854296468ab97a8383b6bc7154184a2
Will <will@i007.cc>
2026-07-07 20:17:50 +0800

    update

--- run_centos.sh
+++ run_centos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 basepath=$(cd `dirname $0`; pwd)
 
-./centos7/check_invalid_user.sh
-./centos7/check_wrong_passwd.sh
+$basepath/centos7/check_invalid_user.sh
+$basepath/centos7/check_wrong_passwd.sh
