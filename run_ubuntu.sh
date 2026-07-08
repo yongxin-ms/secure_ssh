2a82fb4320e07e32c42502bdfa48c343e20828e5
Will <iamwilliam@hotmail.com>
2026-07-08 13:33:24 +0800

    阈值判断改成了5次

--- run_ubuntu.sh
+++ run_ubuntu.sh
@@ -6,7 +6,7 @@ for i in `cat  $logfile`
 do
   IP=`echo $i |awk -F= '{print $1}'`
   NUM=`echo $i|awk -F= '{print $2}'`
-  if [ ${#NUM} -gt 1 ]; then
+  if [ $NUM -gt 5 ]; then
     grep $IP /etc/hosts.deny > /dev/null
     if [ $? -gt 0 ];then
       echo "sshd:$IP:deny" >> /etc/hosts.deny
