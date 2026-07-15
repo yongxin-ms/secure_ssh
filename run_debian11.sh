7e1a7d629050178c74a35b5e0428cf5529498fa6
Debian <azureuser@Debian11.qkjzig4tpdnehodayxxrfm0ebb.ix.internal.cloudapp.net>
2026-07-15 14:57:34 +0800

    update

--- run_debian11.sh
+++ run_debian11.sh
@@ -3,5 +3,6 @@ set -euo pipefail
 
 basepath=$(cd "$(dirname "$0")" && pwd)
 
+"$basepath/debian11/conn_closed_by_invalid_user.sh"
 "$basepath/debian11/invalid_user.sh"
 "$basepath/debian11/disconn_from_invalid_user.sh"
